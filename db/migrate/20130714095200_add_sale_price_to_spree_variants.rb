a569a21afb0438d279b7a1c98a8b795653ceb302
Josh Teng <joshteng@webermize.com>
2013-07-14 18:01:07 +0800

    shoppers can see products on sales

--- db/migrate/20130714095200_add_sale_price_to_spree_variants.rb
+++ db/migrate/20130714095200_add_sale_price_to_spree_variants.rb
@@ -1,5 +1,6 @@
 class AddSalePriceToSpreeVariants < ActiveRecord::Migration
   def change
     add_column :spree_variants, :sale_price, :decimal, :precision => 8, :scale => 2
+    add_index :spree_variants, :sale_price
   end
 end
